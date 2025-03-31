7e4c030c1500885cd576787693b094218ed0eee9
RChaubey16 <ruturajchaubey16@gmail.com>
2025-03-31 18:48:58 +0530

    ui: Add Profile component.

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,21 @@ export default function Home() {
   return (
     <div className="h-[100vh]">
       <div className="w-full max-w-4xl mx-auto">
+        {/* Profile component */}
+        <div>
+          {/* Avatar */}
+          {/* Use shadcn avatar component */}
+          <div className="border border-red-500">
+             {/* Name */}
+             <h3 className="text-xl font-semibold text-white">Ruturaj Chaubey</h3>
+          {/* Designation */}
+          <p className="text-white/80">Software Engineer</p>
+          </div>
+         
+
+        </div>
         {/* Link Cards Container */}
-        <div className="space-y-5">
+        <div className="space-y-5 w-[700px]">
           {links.map((link, index) => (
             <LinkCard
               key={index}
