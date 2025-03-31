831af32453e5332adfada71b480ab25a49f14a37
RChaubey16 <ruturajchaubey16@gmail.com>
2025-03-31 12:34:40 +0530

    ui: Create a LinkCard component.

---
+++ components/LinkCard.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+interface LinkCardProps {
+  logo: string;
+  text: string;
+  description: string;
+  linkHref: string;
+  linkText: string;
+}
+
+export default function LinkCard({
+  logo,
+  text,
+  description,
+  linkHref,
+  linkText,
+}: LinkCardProps) {
+  return (
+    <div className="flex justify-between items-center bg-dark-a10 rounded-lg border border-dark-a30">
+      <div className="p-4 flex items-center">
+        <Image src={logo} alt={"GitHub logo"} height={30} width={30} />
+        <div className="ml-5">
+          <h4 className="text-lg font-semibold text-white">{text}</h4>
+          <p className="text-white/70">{description}</p>
+        </div>
+      </div>
+
+      <Link href={linkHref} target="_blank">
+        <Button
+          variant={"socialLink"}
+          size={"link"}
+          className="mr-4 text-base cursor-pointer"
+        >
+          {linkText}
+        </Button>
+      </Link>
+    </div>
+  );
+}
