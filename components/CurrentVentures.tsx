3b62b9bbd834440039210558ee987f84bf82021c
RChaubey16 <ruturajchaubey16@gmail.com>
2025-06-05 16:45:18 +0530

    chore: Add Current venture data.

--- components/CurrentVentures.tsx
+++ components/CurrentVentures.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import TitleDescription from "./ui/TitleDescription";
+import Link from "next/link";
 
 export default function CurrentVentures() {
   return (
@@ -9,19 +11,45 @@ export default function CurrentVentures() {
         classes="mt-16"
       />
       <div className="mt-10 grid grid-cols-1 gap-4">
-        <div>
-          <h4 className="text-lg text-white/90 font-semibold">
-            Roast My Drupal V2
-          </h4>
-          <p className="text-base text-white/70">
-            Get your Drupal profile roasted
-          </p>
+        <div className="flex items-center gap-4">
+          <Image
+            src={"/project-icon.svg"}
+            alt={"New Project Logo"}
+            width={30}
+            height={30}
+            className="w-[30px] h-[30px] object-cover rounded-lg"
+          />
+          <div>
+            <Link
+              href={"/home"}
+              className="text-lg text-white/90 font-semibold hover:underline"
+            >
+              Roast My Drupal V2
+            </Link>
+            <p className="text-base text-white/70">
+              Get your Drupal profile roasted
+            </p>
+          </div>
         </div>
-        <div>
-          <h4 className="text-lg text-white/90 font-semibold">Curate</h4>
-          <p className="text-base text-white/70">
-            AI powered curation tool for your content
-          </p>
+        <div className="flex items-center gap-4">
+          <Image
+            src={"/project-icon.svg"}
+            alt={"New Project Logo"}
+            width={30}
+            height={30}
+            className="w-[30px] h-[30px] object-cover rounded-lg"
+          />
+          <div>
+            <Link
+              href={"/home"}
+              className="text-lg text-white/90 font-semibold hover:underline"
+            >
+              Curate
+            </Link>
+            <p className="text-base text-white/70">
+              A tool to help you curate content for your audience
+            </p>
+          </div>
         </div>
       </div>
     </div>
